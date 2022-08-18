f56d18568e8f364e3066c7647a2ba6cf4a0485a7
Samuel <kalusam619@gmail.com>
2022-08-18 23:25:03 +0300

    setup the sprites for the planet and the alien weapon

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
 import pygame
 from pygame.locals import *
 
-
+screen_width, screen_height = 600, 500
 pygame.init()
-pygame.display.set_caption('Pygame For 2d Games')
-screen = pygame.display.set_mode((600, 500))
+pygame.display.set_caption('Pygame Planet Invader')
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-BLACK = (0, 0, 0)
+# RED = (255, 0, 0)
+# GREEN = (0, 255, 0)
+# BLUE = (0, 0, 255)
+# BLACK = (0, 0, 0)
 
 
+weapon = pygame.image.load('assets/weapon.png').convert()
+planet = pygame.image.load('assets/planet.png').convert()
+background = pygame.image.load('assets/background.png').convert()
 
-weapon = 'assets/gun.png'
-planet = 'assets/earth.png'
-bg_image = 'assets/background.png'
+
+planet_rect = planet.get_rect()
+weapon_rect = weapon.get_rect()
+background_rect = background.get_rect()
+
+weapon_rect.center = screen_width//2,round(screen_height*5/7)
+
+planet_speed = [2, 0]
+
+# GAME LOOP
+clock = pygame.time.Clock()
 running = True
-background = BLACK
 while running:
     for event in pygame.event.get():
         # print(event)
         if event.type == QUIT:
             running = False
-        
+            
+            
+    # screen.blit(background,background_rect)
+    planet_rect = planet_rect.move(planet_speed)
+    if planet_rect.left < 0 or planet_rect.right > screen_width:
+        planet_speed[0] = -planet_speed[0]
 
-    screen.fill(background)
+    print(planet_rect.size)
+    print(weapon_rect.size)
+    screen.blit(planet, planet_rect)
+    screen.blit(weapon, weapon_rect)
     pygame.display.update()
+    clock.tick(60)
 
 
 pygame.quit()
